0be7e3668d702a2a2437771339df11924f9466f5
jaanonim <mat8mro@gmail.com>
2021-05-13 18:01:24 +0200

    biger page size

--- app/settings.py
+++ app/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 20,
 }
 
 
